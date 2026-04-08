c46e580278a87d7782f5cb50568f8ee3a2a835fa
Jethro Hu <jhu680@student.fuhsd.org>
2026-04-08 13:37:52 -0700

    Actor and World class written (broken)
    Trying to fix the test class in order to test the classes properly.
    
    Will push when it works.

---
+++ src/engine/Actor.java
@@ -0,0 +1,63 @@
+/* Name: Jethro Hu
+ * Date: Apr 7, 2026
+ * Period: 1
+ * Does it work: yes
+ */
+
+package engine;
+
+import javafx.scene.image.ImageView;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Actor extends ImageView{
+	
+	public Actor() {
+		
+	}
+	
+	public void move(double dx, double dy) {
+		this.setX(this.getX()+dx);
+		this.setY(this.getY()+dx);
+	}
+	
+	public World getWorld() {
+		return (World)(this.getParent());
+	}
+	
+	public double getWidth() {
+		return this.getBoundsInParent().getWidth();
+	}
+	
+	public double getHeight() {
+		return this.getBoundsInParent().getHeight();
+	}
+	
+	public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls){
+		List<A> objects = ((World)(this.getParent())).getObjects(cls);
+		ArrayList<A> intersecting = new ArrayList<>();
+		for(A actor:objects) {
+			if(!(actor == this)&&actor.getBoundsInParent().intersects(this.getBoundsInParent())) {
+				intersecting.add(actor);
+			}
+		}
+		
+		return intersecting;
+	}
+	
+	public <A extends Actor> A getOneIntersectingObject(java.lang.Class<A> cls) {
+		List<A> objects = ((World)(this.getParent())).getObjects(cls);
+		for(A actor:objects) {
+			if(!(actor == this)&&actor.getBoundsInParent().intersects(this.getBoundsInParent())) {
+				return actor;
+			}
+		}
+		
+		return null;
+	}
+	
+	public void addedToWorld() {}
+	
+	public abstract void act(long now);
+	
+}
---
+++ src/engine/World.java
@@ -0,0 +1,160 @@
+/* Name: Jethro Hu
+ * Date: Apr 7, 2026
+ * Period: 1
+ * Does it work: yes
+ */
+
+package engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+
+public abstract class World extends Pane{
+	private AnimationTimer timer;
+	private boolean timerRunning;
+	private ArrayList<KeyCode> keysPressed;
+	private boolean widthSet;
+	private boolean heightSet;
+	
+	
+	public void World() {
+		timer = new AnimationTimer() {
+			long past = -1;
+			@Override
+			public void handle(long now) {
+				if(past == -1) {
+					past = now;
+				}else if(now-past>=1e9){
+					act(now);
+					for(int i = 0;i<getChildren().size();i++) {
+						Actor actor = (Actor)(getChildren().get(i));
+						actor.act(now);
+					}
+				}	
+			}
+		};
+		timerRunning = false;
+		keysPressed = new ArrayList<>();
+		widthSet = false;
+		heightSet = false;
+		this.widthProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if(!widthSet&&newValue.doubleValue()>0.0) {
+					widthSet = true;
+					if(heightSet) {
+						onDimensionsInitialized();
+					}
+				}
+				
+			}
+			
+		});
+		
+		this.heightProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if(!heightSet&&newValue.doubleValue()>0.0) {
+					heightSet = true;
+					if(widthSet) {
+						onDimensionsInitialized();
+					}
+				}
+				
+			}
+			
+		});
+		
+		this.sceneProperty().addListener(new ChangeListener<Scene>(){
+
+			@Override
+			public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+				if(newValue !=null) {
+					requestFocus();
+				}
+			}
+			
+		});
+		
+		this.setOnKeyPressed(e->keysPressed.add(e.getCode()));
+		this.setOnKeyReleased(e -> keysPressed.remove(e.getCode()));
+	
+	}
+	
+	public void start() {
+		timer.start();
+		timerRunning = true;
+	}
+	
+	public void stop() {
+		timer.stop();
+		timerRunning = false;
+	}
+	
+	public boolean isStopped() {
+		return timerRunning;
+	}
+	
+	public void add(Actor actor) {
+		//actor.addedToWorld();
+		this.getChildren().add(actor);
+	}
+	
+	public void remove(Actor actor) {
+		ObservableList<Node> children = this.getChildren();
+		for(int i = 0;i<children.size();i++) {
+			if(children.get(i).equals(actor)) {
+				children.remove(i);
+				return;
+			}
+		}
+	}
+	
+	public <A extends Actor> java.util.List<A> getObjects(java.lang.Class<A> cls){
+		List<A> actors = new ArrayList<A>();
+		ObservableList<Node> children = this.getChildren();
+		for(int i = 0;i<children.size();i++) {
+			if(cls.isInstance(children.get(i))){
+				actors.add(cls.cast(children.get(i)));
+			}
+		}
+		return actors;
+	}
+	
+	public <A extends Actor> java.util.List<A> getObjectsAt(double x, double y, java.lang.Class<A> cls){
+		List<A> actors = new ArrayList<>();
+		ObservableList<Node> children = this.getChildren();
+		for(int i = 0;i<children.size();i++) {
+			Actor child = (Actor)(children.get(i));
+			Bounds bound = child.getBoundsInParent();
+			if(bound.contains(x, y)) {
+				actors.add(cls.cast(child));
+			}
+		}
+		return actors;
+	}
+	
+	public boolean isKeyPressed(javafx.scene.input.KeyCode code) {
+		for(int i = 0;i<keysPressed.size();i++) {
+			if(keysPressed.get(i).equals(code)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public abstract void onDimensionsInitialized();
+	
+	public abstract void act(long now);
+	
+}
